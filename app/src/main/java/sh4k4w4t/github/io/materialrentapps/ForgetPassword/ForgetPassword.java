219d730b1e706211061f159c4411c6704fffdb99
shakawat <shakawat_hossain@outlook.com>
2022-11-18 21:29:13 +0600

    no information from developer

--- app/src/main/java/sh4k4w4t/github/io/materialrentapps/ForgetPassword/ForgetPassword.java
+++ app/src/main/java/sh4k4w4t/github/io/materialrentapps/ForgetPassword/ForgetPassword.java
@@ -2,15 +2,29 @@ package sh4k4w4t.github.io.materialrentapps.ForgetPassword;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import sh4k4w4t.github.io.materialrentapps.R;
 
 public class ForgetPassword extends AppCompatActivity {
+    Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_forget_password);
+
+        nextButton= findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ForgetPassword.this,MakeSelection.class));
+            }
+        });
     }
 }
