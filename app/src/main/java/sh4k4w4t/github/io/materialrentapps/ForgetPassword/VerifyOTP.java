75e7e992bb6fb5013365fce48303ec8cb393ecea
shakawat <shakawat_hossain@outlook.com>
2022-11-18 21:59:35 +0600

    no information from developer

--- app/src/main/java/sh4k4w4t/github/io/materialrentapps/ForgetPassword/VerifyOTP.java
+++ app/src/main/java/sh4k4w4t/github/io/materialrentapps/ForgetPassword/VerifyOTP.java
@@ -6,24 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.RelativeLayout;
+import android.widget.Button;
 
+import sh4k4w4t.github.io.materialrentapps.DashBoard.DashBoardActivity;
 import sh4k4w4t.github.io.materialrentapps.R;
 
-public class MakeSelection extends AppCompatActivity {
-    RelativeLayout viaSmsCode;
+public class VerifyOTP extends AppCompatActivity {
+    Button verifyCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_make_selection);
+        setContentView(R.layout.activity_verify_otp);
 
-        viaSmsCode= findViewById(R.id.viaSMSCode);
-        viaSmsCode.setOnClickListener(new View.OnClickListener() {
+        verifyCode= findViewById(R.id.verify_code);
+        verifyCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MakeSelection.this, VerifyOTP.class));
+                startActivity(new Intent(VerifyOTP.this, DashBoardActivity.class));
             }
         });
     }
